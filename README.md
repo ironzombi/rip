6a70afad57bb6da1e2cb5414469b8965b96bd1a0
ironzombi <span@ironzombi.com>
2021-04-21 13:08:59 +0800

    update readme and initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rip
- Ruby IP command 
+ Ruby IP command
+
+ Because I still get:
+ command not found: ip
+
+ well this fixes that.
+
+ Obviously not as functional as ip command on Linux,
+ infact its barely functional at all.
+
+ why does this even need a README.md ?
---
+++ ip.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'socket'
+/* Version 0.01 v0.1 will be initial release  */
+unless ARGV[0]
+  puts "Available Interfaces: \n"
+  Socket.getifaddrs.map do |iface|
+    next unless iface.addr.ipv4?
+    puts "" + iface.name
+    #puts "\Address:          : " + iface.addr.ip_address
+  end
+  return
+end
+
+def ip_all
+  Socket.getifaddrs.map do |iface|
+    next unless iface.addr.ipv4?
+    puts "\nName:             :" + iface.name
+    puts "\nAddress:          :" + iface.addr.ip_address
+  end
+end
+
+case ARGV[0]
+when "a"||"A"
+  ip_all
+else
+  puts "error roror"
+end
