c7028c3b8d756b246901a5a7cbb25d395e17e0eb
ironzombi <span@ironzombi.com>
2021-04-25 16:42:59 +0800

    Update ip.rb
    long comment trimmed

--- ip.rb
+++ ip.rb
@@ -1,24 +1,27 @@
 #!/usr/bin/env ruby
 require 'socket'
-#/* Version 0.01 v0.1 will be initial release  */
-#default behaviour - called with no args list available interfaces
+# /* Version 0.01 v0.1 will be initial release  */
+# default behaviour - called with no args list available interfaces
 unless ARGV[0]
   puts "Available Interfaces: \n"
   Socket.getifaddrs.map do |iface|
     next unless iface.addr.ipv4?
+
     puts "" + iface.name
-    #puts "\Address:          : " + iface.addr.ip_address
+    # puts "\Address:          : " + iface.addr.ip_address
   end
   return
 end
-#when ip l - list the interfaces #TODO make this different than default behaviour
+# when ip l - list the interfaces #TODO make this different than default
+
 def ip_names
   Socket.getifaddrs.map do |ifnames|
     next unless ifnames.addr.ipv4?
     puts "" + ifnames.name
   end
 end
-#when ip a - list the interfaces and Addresses #TODO add netmask, maybe more ?
+# when ip a - list the interfaces and Addresses #TODO add netmask, maybe more ?
+
 def ip_all
   Socket.getifaddrs.map do |iface|
     next unless iface.addr.ipv4?
@@ -26,7 +29,7 @@ def ip_all
     puts "Address:          :" + iface.addr.ip_address
   end
 end
-#main-------
+# main-------
 case ARGV[0]
 when "a","A"
   ip_all
